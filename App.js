150093f454d3197a6a93b54d760b7d7df6441bc0
rokie-web <rokie.web@gmail.com>
2020-05-08 11:38:48 +0200

    Initial commit

---
+++ App.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+class App extends Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return(
+      <h1>Test</h1>
+    )
+  }
+}
+
+export default App;
