15923bee7699329ce6e9b253435080ac7ad36053
rokie-web <rokie.web@gmail.com>
2020-05-08 15:36:31 +0200

    Random Meme Generator (roughly styled)

--- Header.js
+++ Header.js
@@ -3,8 +3,8 @@ import React from 'react'
 function Header() {
   return (
     <header className='HeaderContainer'>
-      <img src='http://www.pngall.com/?p=8254' alt='You mad?' />
-      <h1>Meme Generator</h1>
+      <h1 className='HeaderContainer-headline'>Meme Generator</h1>
+      <img className='HeaderContainer-image' src='https://i.imgur.com/xn4cYl4.png' alt='You mad?' />
     </header>
   )
 }
--- MemeGenerator.js
+++ MemeGenerator.js
@@ -3,12 +3,91 @@ import React, {Component} from 'react'
 class MemeGenerator extends Component {
   constructor() {
     super()
-    this.state = {}
+    this.state = {
+      topText: '',
+      bottomText: '',
+      randomImage: 'https://i.imgflip.com/345v97.jpg',
+      allMemeImgs: [],
+      laoding: false
+    }
+
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  componentDidMount() {
+    this.setState({
+      loading: true
+    })
+
+    fetch('https://api.imgflip.com/get_memes')
+      .then(response => {
+        if (!response.ok) {
+            throw Error(response);
+        }
+        return response;
+      })
+      .then(response => response.json())
+      .then(response => {
+        const {memes} = response.data
+        this.setState({
+          loading: false,
+          allMemeImgs: memes
+        })
+      })
+      .catch(error => {
+          console.log(error);
+      });
+  }
+
+  handleChange(event) {
+    const {name, value} = event.target;
+    this.setState({
+        [name]: value
+      });
+  }
+
+  handleSubmit(event) {
+    const randNum = Math.floor(Math.random() * this.state.allMemeImgs.length)
+    const randMemeImg = this.state.allMemeImgs[randNum].url
+
+    this.setState({
+      randomImage: randMemeImg
+    });
+
+    event.preventDefault();
   }
 
   render() {
     return (
-      <h1>MEME GENERATOR</h1>
+      <div onSubmit={this.handleSubmit}>
+        <form>
+          <input 
+            name='topText'
+            placeholder='Top text'
+            type='text' 
+            value={this.state.topText}
+            onChange={this.handleChange} 
+          />
+          <input 
+            name='bottomText'
+            placeholder='Bottom Text'
+            type='text' 
+            value={this.state.bottomText} 
+            onChange={this.handleChange} 
+          />
+          <button>Gen</button>
+        </form>
+
+        <div className='MemeGeneratorContainer'>
+          <img className='MemeGeneratorContainer-image'
+            src={this.state.loading ? 'loading...' : this.state.randomImage} 
+            alt='Random Meme Image' 
+          />
+          <h2 className='MemeGeneratorContainer-topText'>{this.state.topText}</h2>
+          <h2 className='MemeGeneratorContainer-bottomText'>{this.state.bottomText}</h2>
+        </div>
+      </div>
     )
   }
 }
